d44403b7427cb4a63a6aac700d10ef73dd5e4d10
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-17 20:09:18 -0400

    from pokedexCard navigate to poke details

--- src/components/pokedexPage/PokedexCard.jsx
+++ src/components/pokedexPage/PokedexCard.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 import { useFetch } from "../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 export const PokedexCard = ({ url }) => {
     const [pokemon, getPokemon] = useFetch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         getPokemon(url);
     }, []);
-    
+
+    const handleClick = () => {
+        navigate(`/pokedex/${pokemon.id}`);
+    };
+
     return (
-        <article>
+        <article onClick={handleClick}>
             <img
                 src={pokemon?.sprites.other['official-artwork'].front_default}
                 alt=""
