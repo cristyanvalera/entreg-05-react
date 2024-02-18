a5a28c1bfe3dcd37522b464ca7958f6576c9fe45
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-17 20:07:21 -0400

    in pokeIdPage, now can charge pokemon data from api

--- src/pages/PokeIdPage.jsx
+++ src/pages/PokeIdPage.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useFetch } from "../hooks/useFetch";
+import { useParams } from "react-router-dom";
+
 export const PokeIdPage = () => {
+    const [pokeData, getPokeData] = useFetch();
+    const param = useParams();
+
+    useEffect(() => {
+        let url = `https://pokeapi.co/api/v2/pokemon/${param.id}`;
+        getPokeData(url);
+    }, []);
+
+    console.log(pokeData);
+
     return (
-        <div>PokeIdPage</div>
+        <article>
+            <img
+                src={pokeData?.sprites.other['official-artwork'].front_default}
+                alt={pokeData?.name}
+            />
+            <h3>{pokeData?.name}</h3>
+        </article>
     );
 };
