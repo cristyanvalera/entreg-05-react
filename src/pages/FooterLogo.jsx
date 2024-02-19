16596d0f380a5ddb49f91b2ed8a4df0f637c88d3
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-18 23:44:49 -0400

    created components for header images and footer elements

--- src/pages/FooterLogo.jsx
+++ src/pages/FooterLogo.jsx
@@ -1,12 +1,9 @@
+import footer from '../assets/footer.svg';
+
 export const FooterLogo = () => {
     return (
-        <figure>
-            <svg width="1440" height="154" viewBox="0 0 1440 154" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="1440" height="123.181" fill="#DD1A1A" />
-                <rect y="92" width="1440" height="69" fill="#0C0C0C" />
-                <circle cx="720.5" cy="102.5" r="52.5" fill="white" stroke="black" stroke-width="12" />
-                <circle cx="720.5" cy="102.5" r="25.5" fill="#212121" stroke="black" stroke-width="12" />
-            </svg>
-        </figure>
+        <div>
+            <img src={footer} alt="footer" />
+        </div>
     );
 };
