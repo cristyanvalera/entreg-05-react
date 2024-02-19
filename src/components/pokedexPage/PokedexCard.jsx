830af675f37ea7aeb672a959490d67b0692cdc96
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-18 21:51:34 -0400

    added custom colors for type of the pokemon

--- src/components/pokedexPage/PokedexCard.jsx
+++ src/components/pokedexPage/PokedexCard.jsx
@@ -1,26 +1,49 @@
 import { useEffect } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
+import './styles/pokedexCard.css';
 
 export const PokedexCard = ({ url }) => {
     const [pokemon, getPokemon] = useFetch();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        getPokemon(url);
-    }, []);
+    useEffect(() => getPokemon(url), []);
 
     const handleClick = () => {
         navigate(`/pokedex/${pokemon.id}`);
     };
 
     return (
-        <article onClick={handleClick}>
-            <img
-                src={pokemon?.sprites.other['official-artwork'].front_default}
-                alt="pokemon"
-            />
+        <article onClick={handleClick} className="poke-card">
+            <div className={pokemon?.types[0].type.name}></div>
+            <figure>
+                <img
+                    src={pokemon?.sprites.other['official-artwork'].front_default}
+                    alt="pokemon"
+                />
+            </figure>
+
             <h3>{pokemon?.name}</h3>
+
+            <ul className="poke-types">
+                {pokemon?.types.map(type => (
+                    <li key={type.type.url} className={`slot${type.slot}`}>
+                        {type.type.name}
+                    </li>
+                ))}
+            </ul>
+
+            <p>Type</p>
+            <hr />
+            <ul className="poke-stats">
+                {pokemon?.stats.map(stat => (
+                    !stat.stat.name.includes('special') &&
+                    <li key={stat.stat.url}>
+                        {stat.stat.name}
+                        <span>{stat.base_stat}</span>
+                    </li>
+                ))}
+            </ul>
         </article>
     );
 };
